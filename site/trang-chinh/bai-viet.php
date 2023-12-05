7c63ba15e90e05cff9db53afa71677f0ec1b0df4
caoquoclinh <caoquoclinh.01012004@gmail.com>
2023-12-05 09:57:35 +0700

    Them file xu ly cac chuc nang giao dien

---
+++ site/trang-chinh/bai-viet.php
@@ -0,0 +1,143 @@
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-lg-8">
+            <!-- Post content-->
+            <article>
+                <!-- Post header-->
+                <header class="mb-4">
+                    <!-- Post title-->
+                    <h1 class="fw-bolder mb-1">THE COFFEE HOUSE - DỊCH VỤ GIAO HÀNG MIỄN PHÍ</h1>
+                    <!-- Post meta content-->
+                    <div class="text-muted fst-italic mb-2">Đăng vào ngày 1 tháng 12 năm 2023 bởi Admin</div>
+                    <!-- Post categories-->
+                    <a class="badge text-decoration-none link-light" href="#!">Sản Phẩm</a>
+                    <a class="badge text-decoration-none link-light" href="#!">Mới</a>
+                </header>
+                <!-- Preview image figure-->
+                <figure class="mb-4"><img class="img-fluid rounded" src="../../content/images/bai_viet.png" alt="..." />
+                </figure>
+                <!-- Post content-->
+                <section class="mb-5">
+                    <p class="fs-5 mb-4">
+                        Xin chào các khách hàng thân mến,
+                        <br>
+                        THE COFFEE HOUSE đang có chương trình khuyến mãi khi mua từ 2 sản phẩm quý khách sẽ được miễn
+                        phí hoàn toàn chi phí vận chuyển.
+                        <br>
+                        Trân trọng kính mời quý khách tham gia. Cảm ơn sự tin tưởng của mọi người. THE COFFEE HOUSE xin
+                        cảm ơn!
+                    </p>
+                    <p class="fs-5 mb-4">[Chi tiết Chương trình khuyến mãi]
+                        <br>
+                        Ngày: Thứ hai, 8 giờ tối ngày 27 tháng 11 năm 2023
+                        <br>
+                        Địa điểm: THE COFFEE HOUSE chi nhánh Cần Thơ
+                        <br>
+                        * Duy nhất tại chi nhánh Cần Thơ.
+                    </p>
+                    <!-- <h2 class="fw-bolder mb-4 mt-5">Tôi có những suy nghĩ kỳ quặc về vũ trụ mỗi ngày</h2> -->
+                    <p class="fs-5 mb-4">- Thời gian mua: Từ 7 giờ sáng đến 22 giờ tối.
+                        <br>
+                        - Thời gian diễn ra chương trình: Băt đầu từ Thứ hai, 8 giờ tối ngày 27 tháng 11 năm 2023 -->
+                        Thứ hai, 8 giờ tối ngày 18 tháng 12 năm 2023
+                        <br>
+                        ※ Tất cả khách hàng mua hàng sau 8 giờ tối ngày 27 tháng 11 năm 2023
+                        <br>
+                        ※ Bạn có thể tham gia chương trình khi thanh toán online trên website. Bạn phải đăng nhập vào
+                        THE COFFEE HOUSE bằng tài khoản cá nhân đã được sử dụng để mua sản phẩm.</p>
+                    <p class="fs-5 mb-4">Liên hệ với chúng tôi trên Dịch vụ khách hàng của THE COFFEE HOUSE.</p>
+                </section>
+            </article>
+            <!-- Comments section-->
+            <section class="mb-5">
+                <div class="card bg-light">
+                    <div class="card-body">
+                        <!-- Comment form-->
+                        <form class="mb-4"><textarea class="form-control" rows="3"
+                                placeholder="Vui lòng đăng nhập để bình luận!"></textarea></form>
+                        <!-- Comment with nested comments-->
+                        <div class="d-flex mb-4">
+                            <!-- Parent comment-->
+                            <div class="flex-shrink-0"><img class="rounded-circle"
+                                    src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
+                            <div class="ms-3">
+                                <div class="fw-bold">Nguyễn Thanh Hòa</div>
+                                Điều đó thật tuyệt vời! Tôi rất mong chờ nó.
+                                <!-- Child comment 1-->
+                                <div class="d-flex mt-4">
+                                    <div class="flex-shrink-0"><img class="rounded-circle"
+                                            src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
+                                    <div class="ms-3">
+                                        <div class="fw-bold">Lê Thanh Như Ngọc</div>
+                                        Tôi cũng vậy, thật tò mò về sản phẩm này.
+                                    </div>
+                                </div>
+                                <!-- Child comment 2-->
+                                <div class="d-flex mt-4">
+                                    <div class="flex-shrink-0"><img class="rounded-circle"
+                                            src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
+                                    <div class="ms-3">
+                                        <div class="fw-bold">QTV THE COFFEE HOUSE</div>
+                                        Cảm ơn bạn đã quan tâm!
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Single comment-->
+                        <div class="d-flex">
+                            <div class="flex-shrink-0"><img class="rounded-circle"
+                                    src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
+                            <div class="ms-3">
+                                <div class="fw-bold">Nguyễn Thanh Dương</div>
+                                Sẽ thế nào nếu tôi sở hữu nó😍 Thật tuyệt!
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- Side widgets-->
+        <div class="col-lg-4">
+            <!-- Search widget-->
+            <div class="card mb-4">
+                <div class="card-header">Tìm kiếm</div>
+                <div class="card-body">
+                    <div class="input-group">
+                        <input class="form-control" type="text" placeholder="Nhập từ khóa..."
+                            aria-label="Nhập từ khóa..." aria-describedby="button-search" />
+                        <button class="btn btn-primary" id="button-search" type="button">Tìm</button>
+                    </div>
+                </div>
+            </div>
+            <!-- Categories widget-->
+            <div class="card mb-4">
+                <div class="card-header">Mục lục</div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <ul class="list-unstyled mb-0">
+                                <li>
+                                    <a href="#!">Âm nhạc</a>
+                                </li>
+                                <li><a href="#!">Mẹo vặt</a></li>
+                                <li><a href="#!">Tin tức</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-6">
+                            <ul class="list-unstyled mb-0">
+                                <li><a href="#!">Thời trang</a></li>
+                                <li><a href="#!">Tuyển dụng</a></li>
+                                <li><a href="#!">Giới thiệu</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Side widget-->
+            <div class="card mb-4">
+                <div class="card-header">Xem thêm</div>
+                <div class="card-body">"TẬN HƯỞNG HƠN CÙNG SỔ KHUYẾN MÃI FREEZE TRÀ XANH"</div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ site/trang-chinh/gioi-thieu.php
@@ -0,0 +1,64 @@
+<div class="wrapper" id="wrapper">
+    <div class="" id="banner">
+        <div class="parallax text-center" style="background-image:url(../../content/images/banners/banner_gt.jpg);">
+            <div class="parallax-pattern-overlay">
+                <div class="container text-center text-white" style="height:650px;padding-top:15%;">
+                    <h2 class="text-danger display-2 shadow font-weight-bold">THE COFFEE HOUSE</h2>
+                    <h3 class="text-danger learn shadow font-weight-bold">Về chúng tôi</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="aboutus" id="aboutus">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="heading text">
+                        <!-- <img class="dividerline"
+                            src="http://www.pixempire.com/images/preview/horizontal-line-icon-1.jpg" alt=""> -->
+                        <h2 class="text-success text-center">Nơi dành cho các tính đồ yêu thích coffee và các loại nước
+                            uống</h2>
+                        <!-- <img class="dividerline"
+                            src="http://www.pixempire.com/images/preview/horizontal-line-icon-1.jpg" alt=""> -->
+                        <p>
+                        <h5 class="text-success">NGUỒN GỐC</h5>
+                        THE COFFEE HOUSE được thành lập vào năm 1999, bắt nguồn từ tình yêu dành cho đất Việt cùng
+                        với cà phê và cộng đồng nơi đây. Ngay từ những ngày đầu tiên, mục tiêu của chúng mình là có
+                        thể phục vụ và góp phần phát triển cộng đồng bằng cách siết chặt thêm sự kết nối và sự gắn
+                        bó giữa người với người.
+                        <br>
+                        <br>
+                        <h5 class="text-success">DỊCH VỤ</h5>
+                        THE COFFEE HOUSE là không gian của chúng mình nên mọi thứ ở đây đều vì sự thoải mái của
+                        chúng mình. Đừng giữ trong lòng, hãy chia sẻ với chúng mình điều bạn mong muốn để cùng nhau
+                        giúp THE COFFEE HOUSE trở nên tuyệt vời hơn.
+                        <br>
+                        <br>
+                        <h5 class="text-success">NGHỀ NGHIỆP</h5>
+                        Là điểm hội tụ của cộng đồng, THE COFFEE HOUSE luôn tìm kiếm những thành viên mới với mong muốn
+                        không ngừng hoàn thiện một không gian dành cho tất cả mọi người. Chúng mình luôn chào đón bạn
+                        trở thành một phần của THE COFFEE HOUSE để cùng nhau siết chặt thêm những kết nối và sự gắn bó
+                        giữa người với người.
+                        <p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.829740476067!2d105.77066347412878!3d10.030904790076052!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a0899390fdaf8f%3A0x58b236adf1127e29!2sThe%20Coffee%20House%203%2F2!5e0!3m2!1svi!2s!4v1701611229848!5m2!1svi!2s"
+                        width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+
+</div>
